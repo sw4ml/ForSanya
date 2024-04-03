ce90dbc103a2dbbdad5500d5ecc14a63dbeb3113
sw4ml <165944185+sw4ml@users.noreply.github.com>
2024-04-03 22:38:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задача_1
+{
+    internal class Program
+    {
+        // Первая задачка. Не запускать и подумать Почему? wtf
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 38;
+            int c = (31 - 5 * a) / b;
+            // -19/38 = -0,5. Но тогда нужно вначале С присвоить float либо будет 0. 
+            Console.WriteLine(c);
+        }
+    }
+}
