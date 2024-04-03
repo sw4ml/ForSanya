471df1d1a50c4dbe43521c1c13329c31bba134d8
sw4ml <165944185+sw4ml@users.noreply.github.com>
2024-04-03 21:10:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace first
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOfWeek;
+            Console.WriteLine("Введите день недели");
+
+            dayOfWeek = Console.ReadLine();
+             
+            switch (dayOfWeek) 
+            {
+                case "понедельник":
+                    Console.WriteLine("идем в кино");
+                    break;
+                case "вторник":
+                    Console.WriteLine("идем в бассей");
+                    break;
+
+            }
+         
+
+        }
+    }
+}
